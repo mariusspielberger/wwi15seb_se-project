9358d73edbdfb32cc4c88e15b0e6764ab1822cc0
nzelot <leontsteiner@gmail.com>
2017-06-21 00:24:12 +0200

    Stack setup and running. May need jvm.options adjustments

--- docker-image/startup.sh
+++ docker-image/startup.sh
@@ -1,3 +1,7 @@
-echo 123 | su systemctl start elasticsearch.service
-echo 123 | su systemctl start logstash.service
-echo 123 | su systemctl start kibana.service
+#!/bin/bash
+
+nohup elasticsearch > elastic.stdout.log &
+sleep 1m
+nohup /usr/share/logstash/bin/logstash --path.settings ~/config/ > logstash.stdout.log &
+sleep 1m
+nohup /usr/share/kibana/bin/kibana -c ~/config/kibana.yml > kibana.stdout.log &
