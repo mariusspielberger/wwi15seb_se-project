5c35d120674ad7726738c40552c5e79b569697e5
nzelot <leontsteiner@gmail.com>
2017-06-21 16:09:57 +0200

    One size fits all. Whole Stack in one Container.

--- docker-image/startup.sh
+++ docker-image/startup.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+echo "Elasticsearch"
 nohup elasticsearch > elastic.stdout.log &
+echo "   Waiting for it to Start ..."
 sleep 1m
+echo "Logstash"
 nohup /usr/share/logstash/bin/logstash --path.settings ~/config/ > logstash.stdout.log &
+echo "   Waiting for it to Start ..."
 sleep 1m
+echo "Kibana"
 nohup /usr/share/kibana/bin/kibana -c ~/config/kibana.yml > kibana.stdout.log &
+echo "   Waiting for it to Start ..."
+wait 20s
+echo "Node-RED"
+nohup ~/node_modules/.bin/node-red > node-red.stdout.log &
+echo "   Waiting for it to Start ..."
+wait 10s
+echo "Ready to go. Enjoy :)"
