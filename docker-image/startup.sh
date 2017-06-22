dd2be6a7935feb67b67bf29665ed0d279303a7ad
Marius Spielberger <privat@marius-spielberger.de>
2017-06-22 13:16:24 +0200

    The end user can start the container without closing after the startup script is executed; The nodeRED configuration was added (connection between twitter and logstash)

--- docker-image/startup.sh
+++ docker-image/startup.sh
@@ -13,7 +13,11 @@ nohup /usr/share/kibana/bin/kibana -c ~/config/kibana.yml > kibana.stdout.log &
 echo "   Waiting for it to Start ..."
 sleep 20s
 echo "Node-RED"
-nohup ~/node_modules/.bin/node-red > node-red.stdout.log &
+mkdir ~/.node-red
+cp /tmp/flows_se-project.json ~/.node-red/
+nohup ~/node_modules/.bin/node-red flows_se-project.json > node-red.stdout.log &
 echo "   Waiting for it to Start ..."
 sleep 10s
 echo "Ready to go. Enjoy :)"
+
+read -rsp $'Press escape (ESC) to stop this docker container\n' -d $'\e'
