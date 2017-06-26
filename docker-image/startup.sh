7a2c9bc1ef41527bd86f7aa9384834d40f0069c8
Marius Spielberger <privat@marius-spielberger.de>
2017-06-26 13:14:18 +0200

    Startup corrections

--- docker-image/startup.sh
+++ docker-image/startup.sh
@@ -3,21 +3,20 @@
 echo "Elasticsearch"
 nohup elasticsearch > elastic.stdout.log &
 echo "   Waiting for it to Start ..."
-sleep 1m
+sleep 40s
 echo "Logstash"
 nohup /usr/share/logstash/bin/logstash --path.settings ~/config/ > logstash.stdout.log &
 echo "   Waiting for it to Start ..."
-sleep 1m
+sleep 40s
 echo "Kibana"
 nohup /usr/share/kibana/bin/kibana -c ~/config/kibana.yml > kibana.stdout.log &
 echo "   Waiting for it to Start ..."
 sleep 20s
 echo "Node-RED"
-mkdir ~/.node-red
-cp /tmp/flows_se-project.json ~/.node-red/
-nohup ~/node_modules/.bin/node-red flows_se-project.json > node-red.stdout.log &
+nohup ~/node_modules/.bin/node-red --settings ~/node_modules/node-red/settings.js  flows_se-project.json > node-red.stdout.log &
 echo "   Waiting for it to Start ..."
 sleep 10s
 echo "Ready to go. Enjoy :)"
 
-read -rsp $'Press escape (ESC) to stop this docker container\n' -d $'\e'
+/bin/bash
+#read -rsp $'Press escape (ESC) to stop this docker container\n' -d $'\e'
